19c501d1d5e154d743691f3358f82ca7075cf7f6
adam <gemadam3@gmail.com>
2022-02-08 17:11:54 +0100

    Unit tests

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using APL_Final_Project;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace Tests
+{
+    [TestClass]
+    public class NormalisationTests
+    {
+        [TestMethod]
+        [DataRow("7, 3, 21")]
+        [DataRow("0.9490196, 1, 2.74978519E+30, -7.95935E+29, -5.2852965E+31")]
+        public void ShouldNormalizeValuesToRange_0_1(string values)
+        {
+            float[] arrayToNormalize = values
+                .Split(',')
+                .Select(x => (float)Convert.ToDouble(x))
+                .ToArray();
+
+            var result = BmpUtil.Normalize(arrayToNormalize, arrayToNormalize.Min(), arrayToNormalize.Max());
+
+            Assert.IsFalse(result.Any(x => x > 1), $"Max val is: {result.Max()}");
+            Assert.IsFalse(result.Any(x => x < 0), $"Min val is: {result.Min()}");
+        }
+    }
+}
